e2c44a33030b9898871060bee6430cf114bf7010
wuddupmydawg <fxnoob71@gmail.com>
2018-09-03 20:17:39 +0530

    fast-copy-paste: updating some codes

--- content_script.js
+++ content_script.js
@@ -1,4 +1,31 @@
-var D ={data:null};
+//utils
+function __D(value){
+  this.timeStamp = +new Date;//copying time in miliseconds
+  this.data = value;  // main data
+  this.override = null;//override setting
+}
+function __setData(D){
+  var ret = false;
+  var oldD = __getData();
+  var diffMiliSec = D.timeStamp-oldD.timeStamp;
+  if (diffMiliSec>2000) { 
+    ____D.timeStamp = D.timeStamp;
+    ____D.data = D.data;
+    ret = true;
+  }
+  return ret;
+}
+function __getData(){
+  return ____D;
+}
+function __isNonCopyElements(ele){
+  var ret  = null;
+  if (ele.type=='text'||ele.type=='textarea'||ele.type=='password'||ele.type=='email')
+    ret = true;
+  return ret;  
+}
+//init
+var ____D = new __D(null);
 //getting status of allow copypaste from extensions localStorage using message passing mechanism
 chrome.runtime.sendMessage({method: "settings_fastcp"}, function(response) {
   //alert(response);
@@ -8,25 +35,35 @@ chrome.runtime.sendMessage({method: "settings_fastcp"}, function(response) {
     document.addEventListener("mouseup",ff,false);
     //fast paste
     document.addEventListener("mousemove",mff,false);
+    ____D.override = response.allow_fast_override_status; 
     //cutting the text from document
     document.addEventListener("cut",cff,false);
   }
 });
 
+//utils
+
+
+
+
 //capituring message
-
-
-
 function ff(e)
 {
 
   var data = document.getSelection().toString() ;
-  if(data!=''&&data.length!=0)
-  {
-    D.data= data;
-    document.execCommand('copy');
-    event.preventDefault();
-  }
+  var x = e.clientX;
+  var y = e.clientY;
+  var ele = document.elementFromPoint(x,y);
+  if(!__isNonCopyElements(ele)){
+    if(data!=''&&data.length!=0)
+    {
+      //D.data= data;
+      var temp = new __D(data);
+      if(__setData(temp))
+        document.execCommand('copy'); 
+      event.preventDefault();
+    }
+  } 
 }
 function mff(e)
 {
@@ -34,14 +71,19 @@ function mff(e)
   var y = e.clientY;
   var ele = document.elementFromPoint(x,y);
   var data;
+  if(ele!=null)
   if(ele.type=='text'||ele.type=='textarea'||ele.type=='password'||ele.type=='email')
   {
     ele.addEventListener('dblclick',function(ee){
-
+      var D = __getData();
       if(D.data!=null) {
-        ele.value = D.data;
+        if (D.override = "yes")
+          ele.value = D.data;
+        else {
+          var dat = ele.value +D.data;
+          ele.value=dat;
+        }
         event.preventDefault();
-
       }
     },false);
   }
