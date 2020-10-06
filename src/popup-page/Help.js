b3184c416e2a072b8369994db326782112130513
fxnoob <fxnoob71@gmail.com>
2020-10-06 22:00:14 +0530

    add support form link

--- src/popup-page/Help.js
+++ src/popup-page/Help.js
@@ -7,7 +7,10 @@ export default () => {
       <h3>{i18nService.getMessage("popup_help_heading_str")}</h3>
       <div>
         {i18nService.getMessage("popup_help_desc_str")}{" "}
-        <a target="_blank" href="">
+        <a
+          target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdTB0tFZHTnRe5_L64lYCbJvQm4XD5zGoSubYYYqBEOYXkIGA/viewform?usp=sf_link"
+        >
           {i18nService.getMessage("here")}
         </a>
         .
