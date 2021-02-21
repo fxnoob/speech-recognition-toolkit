eb4465324b7e021ac27bc921557c5f45a1908dd4
fxnoob <fxnoob71@gmail.com>
2021-02-21 12:20:36 +0530

    no special char allowed as key in locales json

--- src/services/guid.js
+++ src/services/guid.js
@@ -3,7 +3,7 @@ guid.generateGuid = () => {
   var result, i, j;
   result = "";
   for (j = 0; j < 32; j++) {
-    if (j == 8 || j == 12 || j == 16 || j == 20) result = result + "-";
+    if (j == 8 || j == 12 || j == 16 || j == 20) result = result + "_";
     i = Math.floor(Math.random() * 16)
       .toString(16)
       .toUpperCase();
